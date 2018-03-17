d77729ad405a2ccf90a0ab4e4c5c2f0b8c362c0e
Yevgeniy <jenyaslu@gmail.com>
2018-03-17 18:29:52 +0200

    Added simple ui layout and logic

--- App.js
+++ App.js
@@ -10,7 +10,10 @@ import {
     Platform,
     StyleSheet,
     Text,
-    View
+    View,
+    Button,
+    Image,
+    Svg
 } from 'react-native';
 import Login from './js/login'
 
@@ -22,24 +25,58 @@ const instructions = Platform.select({
 });
 
 type Props = {};
+
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Izifo!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started please check if you have any new allergies
-        </Text>
-          <Text>Welcome to the Facebook SDK for React Native!</Text>
-                <Login />
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+
+    state = { currentPage: 0 };
+
+    onChangeScreen = (pageIndex) => {
+    console.log(pageIndex)
+        this.setState({ currentPage: pageIndex });
+    }
+
+    takePhoto = () => {
+
+    }
+
+    approveIngridient = (result) => {
+        if (result) {
+            // TODO positive
+        } else {
+            // TODO negative
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                {
+                    this.state.currentPage > 1 ?
+                        <Header style={styles.menu}>
+                            <Body style={styles.menu}>
+                                <Button title="Camera"/>
+                                <Button title="Type"/>
+                            </Body>
+                        </Header>
+                        : null
+                }
+                <Text style={styles.welcome}>
+                    Welcome to Izifo!
+                </Text>
+                {//<Image source={require('./resources/images/logo.png')}/>
+                }
+                <Text style={styles.instructions}>
+                  To get started add allergens or known components that you don't want to have in your purchase
+                </Text>
+
+                <Button title="Add component" onPress={this.onChangeScreen.bind(this, '1') } />
+
+                <Text>Or simply scan product to check it for components</Text>
+
+                <Button title="Scan an item" onPress={this.onChangeScreen.bind(this, '2') } />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
@@ -47,18 +84,33 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
+    marginBottom: 5
   },
+  menu: {
+    flex: 1,
+    backgroundColor: '#1569C7',
+    alignItems: 'stretch',
+    flexDirection: 'row'
+  },
+  notMainPage: {
+    flex: 10
+  },
+  cameraWrapper: {
+    flex: 5
+  },
+  buttonBlock: {
+    flex: 4
+  }
 });
 
 AppRegistry.registerComponent('fbsdk', () => fbsdk);
